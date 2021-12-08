2ba2b7d251ec230060ccfe953ce96b4ca2d39225
stevenovak123 <stevenovak0802@gmail.com>
2021-12-08 20:07:52 +0530

    added daisy ui and created pages and basic ui elements

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Footer from './components/layout/Footer'
 import Navbar from './components/layout/Navbar'
-
+import Home from './pages/Home'
+import About from './pages/About'
+import NotFound from './pages/NotFound'
 function App() {
 	return (
 		<Router>
 			<div className='flex flex-col justify-between h-screen'>
 				<Navbar />
-				<main>Content</main>
+				<main className='mx-auto px-3 pb-12'>
+					<Routes>
+						<Route path='/' element={<Home />} />
+						<Route path='/about' element={<About />} />
+						<Route path='/*' element={<NotFound />} />
+					</Routes>
+				</main>
+				<Footer />
 			</div>
 		</Router>
 	)
