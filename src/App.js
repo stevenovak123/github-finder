48caf264dcb53424166ab436a7ce8225c8608b62
stevenovak123 <stevenovak0802@gmail.com>
2021-12-09 13:28:54 +0530

    created alert context and reducer

--- src/App.js
+++ src/App.js
@@ -5,22 +5,25 @@ import Home from './pages/Home'
 import About from './pages/About'
 import NotFound from './pages/NotFound'
 import { GithubProvider } from './context/github/GithubContext'
+import { AlertProvider } from './context/alert/AlertContext'
 function App() {
 	return (
 		<GithubProvider>
-			<Router>
-				<div className='flex flex-col justify-between h-screen'>
-					<Navbar />
-					<main className='mx-auto px-3 pb-12'>
-						<Routes>
-							<Route path='/' element={<Home />} />
-							<Route path='/about' element={<About />} />
-							<Route path='/*' element={<NotFound />} />
-						</Routes>
-					</main>
-					<Footer />
-				</div>
-			</Router>
+			<AlertProvider>
+				<Router>
+					<div className='flex flex-col justify-between h-screen'>
+						<Navbar />
+						<main className='mx-auto px-3 pb-12'>
+							<Routes>
+								<Route path='/' element={<Home />} />
+								<Route path='/about' element={<About />} />
+								<Route path='/*' element={<NotFound />} />
+							</Routes>
+						</main>
+						<Footer />
+					</div>
+				</Router>
+			</AlertProvider>
 		</GithubProvider>
 	)
 }
