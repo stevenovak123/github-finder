512da55a7484de4df628f41ab37689acb15c2d5f
stevenovak123 <stevenovak0802@gmail.com>
2021-12-09 12:27:35 +0530

    created a reducer for the context

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,12 +1,9 @@
-import { useEffect, useContext } from 'react'
+import { useContext } from 'react'
 import Spinner from '../layout/Spinner'
 import UserItem from './UserItem'
 import GithubContext from '../../context/github/GithubContext'
 const UserResults = () => {
-	const { users, loading, fetchUsers } = useContext(GithubContext)
-	useEffect(() => {
-		fetchUsers()
-	}, [])
+	const { users, loading } = useContext(GithubContext)
 
 	if (!loading) {
 		return (
--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,25 +1,35 @@
-import { createContext, useState } from 'react'
-
+import { createContext, useReducer } from 'react'
+import githubReducer from './GithubReducers'
 const GithubContext = createContext()
 
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 
 export const GithubProvider = ({ children }) => {
-	const [users, setUsers] = useState([])
-	const [loading, setLoading] = useState(true)
+	const initialState = {
+		users: [],
+		loading: false,
+	}
+	const [state, dispatch] = useReducer(githubReducer, initialState)
+	//Testing purposes code
 	const fetchUsers = async () => {
+		setLoading()
 		const response = await fetch(`${GITHUB_URL}/users`, {
 			headers: {
 				Authorization: `token ${GITHUB_TOKEN}`,
 			},
 		})
 		const data = await response.json()
-		setUsers(data)
-		setLoading(false)
+		dispatch({
+			type: 'GET_USERS',
+			payload: data,
+		})
 	}
+	const setLoading = () => dispatch({ type: 'SET_LOADING' })
 	return (
-		<GithubContext.Provider value={{ users, loading, fetchUsers }}>
+		<GithubContext.Provider
+			value={{ users: state.users, loading: state.loading, fetchUsers }}
+		>
 			{children}
 		</GithubContext.Provider>
 	)
