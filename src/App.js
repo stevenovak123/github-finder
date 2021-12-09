42aac8a093c27cb311116c3faf2ebd9a2acffe19
stevenovak123 <stevenovak0802@gmail.com>
2021-12-09 13:42:13 +0530

    created Alert context,reducer and alert component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Footer from './components/layout/Footer'
 import Navbar from './components/layout/Navbar'
+import Alert from './components/layout/Alert'
 import Home from './pages/Home'
 import About from './pages/About'
 import NotFound from './pages/NotFound'
@@ -14,6 +15,7 @@ function App() {
 					<div className='flex flex-col justify-between h-screen'>
 						<Navbar />
 						<main className='mx-auto px-3 pb-12'>
+							<Alert />
 							<Routes>
 								<Route path='/' element={<Home />} />
 								<Route path='/about' element={<About />} />
